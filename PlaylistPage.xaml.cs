d8c671c06508f66b81bc2c765269ccca79a3499c
Feezmen <145434551+NikolaiSaharov@users.noreply.github.com>
2025-03-24 14:46:20 +0300

    Update PlaylistPage.xaml.cs

--- PlaylistPage.xaml.cs
+++ PlaylistPage.xaml.cs
@@ -1,40 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FirstTask
 {
-    /// <summary>
-    /// Логика взаимодействия для PlaylistPage.xaml
-    /// </summary>
     public partial class PlaylistPage : Page
     {
         public PlaylistPage()
         {
             InitializeComponent();
             InitializeTrackSelection();
+            InitializeEllipsisButton();
         }
+
         private void InitializeTrackSelection()
         {
             foreach (var track in TracksStackPanel.Children)
             {
                 if (track is Border border)
                 {
+                    border.MouseEnter += Track_MouseEnter;
+                    border.MouseLeave += Track_MouseLeave;
                     border.MouseLeftButtonDown += Track_MouseLeftButtonDown;
                 }
             }
         }
+
+        private void InitializeEllipsisButton()
+        {
+            // Находим StackPanel, содержащую Border с изображением
+            var mainGrid = (Grid)this.Content;
+            var leftStackPanel = (StackPanel)mainGrid.Children[0];
+            var imageBorder = (Border)leftStackPanel.Children[0];
+
+            imageBorder.MouseEnter += (s, e) => EllipsisButton.Opacity = 1;
+            imageBorder.MouseLeave += (s, e) => EllipsisButton.Opacity = 0;
+        }
+
+        private void Track_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is Border border)
+            {
+                border.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#090909"));
+            }
+        }
+
+        private void Track_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is Border border)
+            {
+                border.Background = Brushes.Transparent;
+            }
+        }
+
         private void Track_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if (sender is Border selectedTrack)
@@ -49,7 +68,7 @@ namespace FirstTask
                 }
 
                 // Подсветка выбранного трека
-                selectedTrack.Background = new SolidColorBrush(Colors.LightGray);
+                selectedTrack.Background = new SolidColorBrush(Colors.DarkGray);
             }
         }
     }
