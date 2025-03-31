6c326aaee1d3b80e88dc2e5a421613ba3b11be82
Feezmen <145434551+NikolaiSaharov@users.noreply.github.com>
2025-03-31 13:43:28 +0300

    ну тут исправил немного

--- Otvet.xaml.cs
+++ Otvet.xaml.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.Windows.Data;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace FirstTask
 {
@@ -26,22 +27,6 @@ namespace FirstTask
         }
     }
 
-    public class HalfValueConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is double size)
-            {
-                return size / 2;
-            }
-            return 0;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
     public class CenterPointConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
@@ -59,4 +44,5 @@ namespace FirstTask
         }
     }
 
+    
 }
--- ProfilePage.xaml.cs
+++ ProfilePage.xaml.cs
@@ -3,16 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
 
 namespace FirstTask
 {
     public partial class ProfilePage
     {
+        private bool isSubscribed = false;
+        private int subscribersCount = 0;
+
         public ProfilePage()
         {
             InitializeComponent();
         }
 
-        // Дополнительный код вашей страницы
+        private void SubscribeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isSubscribed)
+            {
+                // Отписываемся
+                SubscribeButton.Content = "Подписаться";
+                subscribersCount--;
+
+                // Возвращаем исходную ширину и полную непрозрачность
+                SubscribeButton.Width = 170;
+                SubscribeButton.Opacity = 1;
+                SubscribeButton.Foreground = new SolidColorBrush(Colors.White);
+            }
+            else
+            {
+                // Подписываемся
+                SubscribeButton.Content = "Вы подписаны";
+                subscribersCount++;
+
+                // Увеличиваем ширину кнопки и добавляем прозрачность
+                SubscribeButton.Width = 190;
+                SubscribeButton.Opacity = 0.7;
+                SubscribeButton.Foreground = new SolidColorBrush(Color.FromArgb(0xB3, 0xFF, 0xFF, 0xFF)); // 70% прозрачности
+            }
+
+            isSubscribed = !isSubscribed;
+        }
     }
 }
